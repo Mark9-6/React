eb0d7edc63ad5495df847d1e79f2840addf13da7
Swastik <98447347+Mark9-6@users.noreply.github.com>
2024-03-06 15:32:28 +0000

    r

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,31 +4,47 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [counter, setCounter] = useState(15) // usestate is a HOOK so that it can update value in user interface
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  //  let counter  = 5;
+
+  const addValue = () => {
+    console.log("clicked", counter)
+    // counter +=1;
+    if (counter<20) {
+        setCounter(counter + 1);
+    }
+  
+  }
+ 
+
+  const removeValue = () => {
+    console.log("clicked , counter", counter) 
+    // counter +=1;
+    if (counter>0) {
+       setCounter(counter - 1);
+    }
+  
+  }
+
+
+  // hook update the value in ui otherwise it remains the same 
+
+
+  return (<>
+    <h1>chai aur code </h1>
+    <h2>counter value : {counter}</h2>
+    <button
+      onClick={addValue}
+    >Add value{counter} </button>
+    <br></br>
+    <button
+      onClick={removeValue}
+    >Remove value{counter}</button>
+    <p>footer: {counter}</p>
+
+
+  </>
   )
 }
 
